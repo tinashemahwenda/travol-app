2e9f220c24646a6ce078023e2020eb1e29652d45
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2024-12-15 15:52:27 +0845

    Fixed scrollview error

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -9,90 +9,93 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
-            child: Text(
-              'Discover new \nExperience',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 40,
+        body: SingleChildScrollView(
+      child: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
+              child: Text(
+                'Discover new \nExperience',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 40,
+                ),
               ),
             ),
-          ),
-          Container(
-            padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
-            decoration: BoxDecoration(),
-            child: TextField(
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(), hintText: 'Search places'),
+            Container(
+              padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+              decoration: BoxDecoration(),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), hintText: 'Search places'),
+              ),
             ),
-          ),
-          SubHeading(
-            title: 'Top Places',
-          ),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 20.0),
-                  child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20),
-                      child: PlacesCard(
-                        placeName: 'Skyline Hotel',
-                        placeLocation: 'Nyanga, Zimbabwe',
-                        imageLocation: 'assets/images/bg-images_1.png',
-                      )),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 20.0),
-                  child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20),
-                      child: PlacesCard(
-                        placeName: 'Umfuridzi',
-                        placeLocation: 'Madziva, Zimbabwe',
-                        imageLocation: 'assets/images/bg-images_2.png',
-                      )),
-                ),
-              ],
+            SubHeading(
+              title: 'Top Places',
             ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          SubHeading(
-            title: 'Top Countries',
-          ),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: [
-                CountriesBubble(
-                  country_name: 'Zimbabwe',
-                ),
-                CountriesBubble(
-                  country_name: 'Rwanda',
-                ),
-                CountriesBubble(
-                  country_name: 'South Africa',
-                ),
-                CountriesBubble(
-                  country_name: 'Zambia',
-                ),
-                CountriesBubble(
-                  country_name: 'Namibia',
-                ),
-              ],
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20.0),
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: PlacesCard(
+                          placeName: 'Skyline Hotel',
+                          placeLocation: 'Nyanga, Zimbabwe',
+                          imageLocation: 'assets/images/bg-images_1.png',
+                        )),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20.0),
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: PlacesCard(
+                          placeName: 'Umfuridzi',
+                          placeLocation: 'Madziva, Zimbabwe',
+                          imageLocation: 'assets/images/bg-images_2.png',
+                        )),
+                  ),
+                ],
+              ),
             ),
-          )
-        ],
+            SizedBox(
+              height: 20,
+            ),
+            SubHeading(
+              title: 'Top Countries',
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  CountriesBubble(
+                    country_name: 'Zimbabwe',
+                  ),
+                  CountriesBubble(
+                    country_name: 'Rwanda',
+                  ),
+                  CountriesBubble(
+                    country_name: 'South Africa',
+                  ),
+                  CountriesBubble(
+                    country_name: 'Zambia',
+                  ),
+                  CountriesBubble(
+                    country_name: 'Namibia',
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     ));
   }
