60b25b969c3a1d203cd24b1bf51a3ce3e8753ffc
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2024-12-15 15:09:16 +0845

    Made top countries bubble

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -92,6 +92,31 @@ class HomePage extends StatelessWidget {
           SubHeading(
             title: 'Top Countries',
           ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20.0, 0, 10, 0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[400],
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Zimbabwe',
+                  style: TextStyle(
+                    fontSize: 12,
+                  ),
+                )
+              ],
+            ),
+          )
         ],
       ),
     ));
